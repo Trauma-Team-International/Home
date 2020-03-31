19a60fc21ce4a6c244093ee488771f35bfbc4b4f
Mikko Kotila <mailme@mikkokotila.com>
2020-03-31 18:21:31 +0300

    adden snapshot based simulation

--- icu_burden/icu_burden_forecast_a.py
+++ icu_burden/icu_burden_forecast_a.py
@@ -1,3 +1,57 @@
+def icu_burden_snapshot(days=30,
+                        day_total_cases=50,
+                        doubles_in_days=6,
+                        case_fatality_rate=.4,
+                        capacity=200,
+                        summaries=True):
+
+    import numpy as np
+    import pandas as pd
+    
+    total_cases = []
+
+    for day in range(days):
+        
+        # count how many new cases there will be today
+        if day % doubles_in_days == 0:
+            _temp_new_ = day_total_cases / doubles_in_days
+        day_new_cases = _temp_new_
+    
+        day_total_cases = day_total_cases + day_new_cases
+    
+        if day_total_cases > capacity:
+            day_total_admitted = capacity
+        else:
+            day_total_admitted = day_total_cases
+    
+        total_cases.append(day_total_cases)
+    
+    df = pd.DataFrame()
+    
+    random_effect = np.round(np.random.normal(0, .2, days), 2)
+    
+    df['cases'] = np.array(total_cases) * random_effect
+    
+    admissions = lambda x: x if capacity >= x else capacity
+    df['admissions'] = df['cases'].apply(admissions).astype(int)
+    
+    expired_cfr = lambda x: x * case_fatality_rate
+    df['expired_cfr'] = df['admissions'].apply(expired_cfr)
+    
+    df['recovered'] = df['admissions'] - df['expired_cfr']
+    df['expired_capacity'] = abs(df['cases'] - df['admissions'])
+    
+    df['doubles_in_days'] = doubles_in_days
+    df['case_fatality_rate'] = case_fatality_rate
+    df['capacity'] = capacity
+    
+    if summaries:
+        return df.astype(int).max().tolist()
+    
+    else:
+        return df.astype(int)
+
+ 
 def create_combinations(params):
     
     '''Takes as input the input arguments from simulate() and 
@@ -7,176 +61,39 @@ def create_combinations(params):
     import itertools
     
     capacity = params['capacity']
-    capacity = range(capacity[0], capacity[1], capacity[2])
-    
     doublers = params['doubles_in_days']
-    doublers = range(doublers[0], doublers[1], doublers[2])
-    
-    mean_duration = params['mean_duration']
-    durations = range(mean_duration[0], mean_duration[1], mean_duration[2])
-    
     case_fatality_rate = params['case_fatality_rate']
+    
+    capacity = range(capacity[0], capacity[1], capacity[2])
+    doublers = range(doublers[0], doublers[1], doublers[2])
     case_fatality_rates = np.arange(case_fatality_rate[0], case_fatality_rate[1], case_fatality_rate[2]).tolist()
     
-    combinations = list(itertools.product(*[capacity, doublers, durations, case_fatality_rates]))
+    combinations = list(itertools.product(*[capacity, doublers, case_fatality_rates]))
 
     return combinations
-
-
-def compute_expired(day_total_cases, case_fatality_rate, mean_duration):
-    
-    import math
-    
-    day_expired_cases = day_total_cases * (case_fatality_rate / mean_duration)
-    
-    return int(math.ceil(day_expired_cases))
-
-
-def compute_new(day, day_total_cases, doubles_in_days):
-    
-    day_new_cases = day_total_cases * (.8 / doubles_in_days)
-        
-    return int(day_new_cases)
     
 
-def simulate(params, randomize=False, debug_limit=None):
+def simulate(params, randomize=False, limit=None):
     
-    '''
-    
-    params | dict | parameter dictionary where each input is a list with start, stop and step values
-    
-    EXAMPLE:
-    
-    params = {'capacity': [150, 1000, 50],
-              'doubles_in_days': [2, 6, 1],
-              'mean_duration': [17, 23, 1],
-              'case_fatality_rate': [0.4, 0.6, 0.01]}
-    
-    results = simulate(params)
-    
-    - each combination runs for 30 days
-    - `capacity` is constant through days
-    - `doubles_in_days` is constant through days
-    - `mean_duration` is constant through days
-    - daily cases grow based on `doubles_in_days`
-    - each day death happens according to `case_fatality_rate`
-    - if capacity is exceeded, more death results
-    - every day `mean_duration` is used for deciding how many recover
 
-    '''
-
-    import uuid
-    import math
-    import random
-    import pandas as pd
-    import numpy as np
     from tqdm import tqdm
     
-    # create all possible combinations from inputs
     combinations = create_combinations(params)
 
     if randomize:
         np.random.shuffle(combinations)
     
-    # initiate output
     out = []
     
-    # program loop
-    
-    for combination in tqdm(combinations[:debug_limit]):
-
-        # combination starts
+    for combination in tqdm(combinations[:None]):
         
-        days = 30
-        day_total_cases = 41
-        
-        capacity = combination[0]
-        doubles_in_days = combination[1]
-        mean_duration = combination[2]
-        case_fatality_rate = combination[3]
-
-        # reset outputs
-        demand_cases = []
-        total_cases = []
-        new_cases = []
-        expired_cases = []
-        
-        for day in range(days):
-            
-            '''
-            - [x] there is a certain number of patients at start `day_total_cases`
-            - [x] each day there are more new patients based on `doubles_in_days`
-            - [x] each day some patients will die based on `case_fatality_rate`
-            - [x] each day some patients are released based on `mean_duration`
-              [x]- all patients that meet `mean_duration` and did not die are released
-            - some patients can be admitted based on current available capacity
-              - such patients will be added to total patients
-            - some patients may not be admittable due to lack of current capacity
-              - such patients will die
-
-            '''
-            
-            if day != 0:
-                
-                # use previous day's end number as base
-                day_total_cases = total_cases[-1]
-       
-            # count how many new cases there are
-            day_new_cases = compute_new(day, day_total_cases, doubles_in_days)
-
-            # use previous day end total cases as base
-            day_expired_cases = compute_expired(day_total_cases, case_fatality_rate, mean_duration)
-
-            # end of day total cases before releasing patients
-            day_total_cases = day_total_cases + day_new_cases - day_expired_cases
-            
-            # release patients
-            if day > mean_duration:
-                day_total_cases = day_total_cases - (new_cases[-mean_duration] - day_expired_cases)
-            
-            # end of day total demand
-            day_demand_cases = day_total_cases + day_new_cases
-            
-            # add case data to combination outputs
-            demand_cases.append(int(day_demand_cases))
-            new_cases.append(int(day_new_cases))
-            total_cases.append(int(day_total_cases))
-            expired_cases.append(int(day_expired_cases))
-            
-            # day ends
-        
-        patient_id = random.randint(1000000000000, 9999999999999)
-        
-        out.append([patient_id,
-                    max(demand_cases),   # get the peak demand
-                    max(total_cases),    # get the peak actual cases (concurrent ICU stays)
-                    sum(new_cases),      # get the total number of new cases
-                    sum(expired_cases),  # get the total number of those expired
-                    combination[0],
-                    combination[1],
-                    combination[2],
-                    combination[3]])
-                   
-        # combination ends
+        out.append(icu_burden_snapshot(capacity=combination[0],
+                                       doubles_in_days=combination[1],
+                                       case_fatality_rate=combination[2]))
         
     df = pd.DataFrame(out)
-    df.columns = ['id',
-                  'demand_cases',
-                  'total_cases',
-                  'new_cases',
-                  'expired_cases',
-                  'capacity',
-                  'doubles_in_days',
-                  'mean_duration',
-                  'case_fatality_rate']
-                   
+    df.columns = ['cases', 'admissions', 'expired_cfr',
+                   'recovered', 'expired_capacity',
+                   'doubles_in_days', 'case_fatality_rate', 'capacity']
+    
     return df
-
-# total_demand
-
-params = {'capacity': [150, 500, 10],
-          'doubles_in_days': [5, 8, 1],
-          'mean_duration': [17, 23, 1],
-          'case_fatality_rate': [0.4, 0.6, 0.01]}
-#results = simulate(params, randomize=True, debug_limit=1)
-results = simulate(params)
